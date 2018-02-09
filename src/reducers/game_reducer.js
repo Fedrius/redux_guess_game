3b222da29777896925ac8e17e975530e31f7ee9f
Paul <paulmlee7@gmail.com>
2018-02-08 17:59:13 -0800

    completed basic func

--- src/reducers/game_reducer.js
+++ src/reducers/game_reducer.js
@@ -9,6 +9,20 @@ const DEFAULT_STATE = {
 
 export default function(state=DEFAULT_STATE, action){
     switch(action.type){
+        case types.GET_RAN_NUMBER:
+            return {...state, randomNumber: action.payload};
+        case types.USER_INPUT:
+            return {...state, userGuess: action.payload};
+        case types.MAKE_GUESS:
+            let message = 'YA GOT IT!!!';
+
+            if(state.randomNumber > state.userGuess){
+                message = 'TOO LOW';
+            } else if(state.randomNumber < state.userGuess){
+                message = 'tooooo high';
+            }
+
+            return {...state, userGuess: '', message, numberOfGuesses: state.numberOfGuesses + 1};
         default:
             return state;
     }
